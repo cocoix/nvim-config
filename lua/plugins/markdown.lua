9a2ffa42775604fe4e2d87e062a221866eacd687
cocoix <cocoix00@gmail.com>
2026-09-01 21:40:45 +0800

    修复图标宽度问题

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -11,6 +11,26 @@ return {
       completions = {
         lsp = { enabled = true }
       },
+      heading = {
+        -- PUA glyphs are configured as double-width, so a trailing space
+        -- would make sign_text three cells wide and Neovim would reject it.
+        signs = { "󰫎" },
+      },
+    },
+  },
+  {
+    "obsidian-nvim/obsidian.nvim",
+    version = "*", -- use latest release, remove to use latest commit
+    ---@module 'obsidian'
+    ---@type obsidian.config
+    opts = {
+      legacy_commands = false, -- this will be removed in 4.0.0
+      workspaces = {
+        {
+          name = "Moss",
+          path = "~/Notes/Moss",
+        },
+      },
     },
   }
 }
