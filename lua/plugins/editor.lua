c11a033ce16b72e8d56bc33ea248086720dbf9db
cocoix <cocoix00@gmail.com>
2026-09-08 23:44:19 +0800

    新增 mini.ai

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "nvim-mini/mini.ai",
+    version = false,
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     opts = {}
