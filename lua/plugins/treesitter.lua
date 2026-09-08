3b8bd0dc80feb1beddedca6fe782e2177939a668
cocoix <cocoix00@gmail.com>
2026-09-09 03:19:38 +0800

    修改 treesitter 默认安装的 parser

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,20 @@
 return {
   {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     lazy = false,
-    build = ':TSUpdate'
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").install({
+        lua,
+        c,
+        cpp,
+        nix,
+        markdown,
+        rust,
+        bash,
+        fish,
+        python,
+      })
+    end,
   },
 }
