241d8d75a9daa3131599ed4ec2c0c9bf2c8eff59
cocoix <cocoix00@gmail.com>
2026-09-08 23:47:21 +0800

    删除 treesitter-textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,13 +4,4 @@ return {
     lazy = false,
     build = ':TSUpdate'
   },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    branch = "main",
-    init = function()
-      -- Disable entire built-in ftplugin mappings to avoid conflicts.
-      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
-      vim.g.no_plugin_maps = true
-    end,
-  }
 }
